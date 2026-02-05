14da02c1b276b065add9f882d8173e4448b11f92
SnakeX Foundation <ppedrovitoor63@gmail.com>
2026-02-05 12:10:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+document.querySelectorAll(".card").forEach(card => {
+  observer.observe(card);
+});
